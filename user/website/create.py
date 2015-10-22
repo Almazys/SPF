2301577c94e95914ed250ea4778edae2c3716010
Almazys <informatique.lma@gmail.com>
2015-10-22 23:17:18 +0200

    Added option to extend new class file

--- user/website/create.py
+++ user/website/create.py
@@ -5,7 +5,7 @@ import re
 
 # check if an argument were given 
 if len(sys.argv) < 2:
-	print("Usage: " + sys.argv[0] + " path/to/controller [author]")
+	print('Usage: ' + sys.argv[0] + ' path/to/controller [author]')
 	sys.exit()
 
 if len(sys.argv) == 3:
@@ -25,20 +25,59 @@ destPath = os.getcwd() + '/'
 if len(sys.argv[1].split('/'))>1:
 	destPath = destPath + '/'.join(sys.argv[1].split('/')[:-1]).lower() + '/'
 
-print("[*] Checking folders...")
+print('[*] Checking folders...')
 if not os.path.exists(destPath):
-	print(" [~] Creating folders ...")
+	print(' [~] Creating folders ...')
 	os.makedirs(destPath)
 
-print("[*] Checking file...")
+print('[*] Checking file...')
 if os.path.isfile(destPath + destFile):
-	answer = input(" [~] File " + destFile + " already exists. Overwrite ? [Y/N] ")
+	answer = input(' [?] File ' + destFile + ' already exists. Overwrite ? [Y/N] ')
 	if (answer!='Y') & (answer!='y'):
-		print("Skipping...")
+		print('Skipping...')
 		sys.exit()
 
+answer = input('[?] Do you want your controller to extend from a "Master Controller" ? [Y/N] ')
+if (answer!='Y') & (answer!='y'):
+	parent="CoreController"
+else:
+	parent="WebsiteController"
+
+if (not os.path.isfile(os.getcwd() + '/../application/WebsiteController.class.php')) & (parent == "WebsiteController"):
+	# Writing WebsiteController class file
+	print('[*] Creating class file ' + os.getcwd() + '/../application/WebsiteController.class.php' + '... ', end='')
+
+	try:
+		with open(os.getcwd() + '/../application/WebsiteController.class.php', 'w') as file:
+			file.write(
+	'''<?php
+
+abstract class WebsiteController extends CoreController {
+
+	public function __construct() {
+		parent::__construct();
+		Debug::write("Building common WebsiteController ...", 0);
+		
+		/**
+		 * Do common stuff concerning your website here
+		 */
+	}
+
+}
+
+?>''')
+
+	except IOError as e:
+		print("failed to write file !")
+		print(str(e))
+		sys.exit()
+
+	print("success !")
+
+
+
 # Writing into new class file
-print("[*] Creating class file " + destPath + destFile + '... ', end="")
+print('[*] Creating class file ' + destPath + destFile + '... ', end='')
 
 try:
 	with open(destPath + destFile, 'w') as file:
@@ -50,7 +89,7 @@ try:
  * @author: ''' + author + '''
  */
 
-class ''' + destFile.replace(".class.php", "") + ''' extends CoreController {
+class ''' + destFile.replace(".class.php", "") + ''' extends ''' + parent + ''' {
 
 	public function __construct() {
 		parent::__construct();
@@ -59,18 +98,17 @@ class ''' + destFile.replace(".class.php", "") + ''' extends CoreController {
 
 	public function work() {
 		# Do stuff here ...
-		# self::$view->setTemplate("specific_template");
-		# self::$view->setLocale("en");
+		# $this->view->setTemplate("specific_template");
+		# $this->view->setLocale("en");
 	}
 
 }
 
 ?>
 ''')
-		file.close()
 
 except IOError as e:
-	print("failed !")
+	print("failed to write file !")
 	print(str(e))
 	sys.exit()
 
